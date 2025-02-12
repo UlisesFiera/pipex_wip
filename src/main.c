13f55798dbc35572ba84d6fa76ab97d282edb5ea
Ulises Fernandez Del Campo Jorquera <ulfernan@c2r19s1.42madrid.com>
2025-02-12 17:54:14 +0100

    v0.2wip

--- src/main.c
+++ src/main.c
@@ -6,13 +6,43 @@
 /*   By: ulfernan <ulfernan@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 18:36:03 by ulfernan          #+#    #+#             */
-/*   Updated: 2025/02/11 18:36:03 by ulfernan         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:54:05 by ulfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_lib.h"
 
-int	main
+void	execve_prep(char **cmd, char **env)
+{
+	
+}
+
+void	child(int *buffer, char **cmd, char **env)
+{
+	int	fd;
+
+	fd = open(cmd[1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	dup2(fd, 0);
+	dup2(buffer[1], 1);
+	close(buffer[0]);
+	execve_prep(cmd[2], env);
+}
+
+main	(int argc, char **argv, char **env)
+{
+	int		buffer[2];
+	pid_t	process;
+
+	pipe(buffer);
+	process = fork();
+	if (process < 0)
+		return (perror("Error\n"));
+	if (!process)
+		child(buffer, argv, env);
+	else
+		parent
+
+}
 
 
 /*
@@ -33,4 +63,12 @@ dup2() interchanges two fd, and it can be used to for, say "now, the stdin is th
 execve() takes what the current program is doing with another; you pass it the
 	args like a command line
 	
+The open() commands write, create if doesn't exist or truncate to 0 if does (erases what's in it). 
+	0777 is for giving permissions as in chmod
+
+1. We take the first argument and initialize it as a file to write to.
+2. We set the stdin and stdout to point to the argv[1] (the first arg) and buffer[1] (the write pipe).
+	We do this because our parent will read the stdout of the write pipe fd. 
+
+
 */
--- src/pipex_lib.h
+++ src/pipex_lib.h
@@ -6,17 +6,17 @@
 /*   By: ulfernan <ulfernan@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 18:36:07 by ulfernan          #+#    #+#             */
-/*   Updated: 2025/02/11 18:36:07 by ulfernan         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:38:32 by ulfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_LIB_H
-# define PUSH_LIB_H
+#ifndef PIPEX_LIB_H
+# define PIPEX_LIB_H
 
 # include <sys/types.h>
 # include <unistd.h>
 
 
-# include "/pipex_wip/libft/libft.h"
+# include "../libft/libft.h"
 
 #endif
